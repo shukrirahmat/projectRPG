aa9e70e8444df751a0279d8758e47f421787049c
Shukri <119567950+shukrirahmat@users.noreply.github.com>
2026-01-29 04:08:46 +0800

    initial commit

---
+++ main.lua
@@ -0,0 +1,125 @@
+function love.load()
+
+    windowWidth = 800
+    windowHeight = 600
+
+    love.graphics.setBackgroundColor(0,0,0)
+    font_small = love.graphics.newFont('fonts/MartianMono-Regular.ttf', 14)
+    font_medium = love.graphics.newFont('fonts/MartianMono-Regular.ttf', 18)
+    font_large = love.graphics.newFont('fonts/MartianMono-Regular.ttf', 24)
+    goblin_sprite = love.graphics.newImage('images/goblin.png')
+    monsterSpriteDimension = 128
+
+    character1 = {
+        name = 'HERO',
+        maxHp = 500,
+        maxMp = 350,
+        attack = 120,
+        defense = 80,
+        speed = 100
+    }
+
+    enemy1 = {
+        name = 'GOBLIN',
+        maxHp = 250,
+        maxMp = 0,
+        attack = 80,
+        defense = 50,
+        speed = 60,
+        sprite = goblin_sprite
+    }
+
+    mainMenu = {'FIGHT', 'FLEE'}
+    mainMenuHighlighted = 1;
+end
+
+function love.draw()
+
+    -----------------------------------------------------
+    local topBoxX = 10
+    local topBoxY = 10
+    local topBoxWidth = 150
+    local topBoxHeight = 100
+    local nameX = topBoxX + 5
+    local nameY = topBoxY + 5
+    local nameWidth = topBoxWidth - 10
+    local hpX = topBoxX + 5
+    local hpY = topBoxY + 25
+    local hpWidth = topBoxWidth - 10
+    local mpX = topBoxX + 5
+    local mpY = topBoxY + 25 * 2
+    local mpWidth = topBoxWidth - 10
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.rectangle('line', topBoxX, topBoxY, topBoxWidth, topBoxHeight)
+    love.graphics.setFont(font_small)
+    love.graphics.printf(character1.name, nameX, nameY, nameWidth, 'center')
+    love.graphics.setFont(font_large)
+    love.graphics.printf('HP '..character1.maxHp..'', hpX, hpY, hpWidth, 'center')
+    love.graphics.printf('MP '..character1.maxMp..'', mpX, mpY, mpWidth, 'center')
+
+    -------------------------------------------
+
+    love.graphics.draw(
+        enemy1.sprite,
+        windowWidth/2, 
+        windowHeight/2,
+        0,
+        1,
+        1,
+        monsterSpriteDimension/2,
+        monsterSpriteDimension/1.5
+    )
+
+    -------------------------------------------
+
+    local bottomBoxHeight = 180
+    local bottomBoxX = 10
+    local bottomBoxY = windowHeight - bottomBoxHeight - 10
+    local bottomLeftWidth = 200
+    local menuOptionX = bottomBoxX + 10
+    local menuOptionY = bottomBoxY + 10
+    local menuOptionWidth = bottomLeftWidth - 20
+    local menuOptionHeight = (bottomBoxHeight - 20)/4
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.rectangle(
+        'line',
+        bottomBoxX,
+        bottomBoxY,
+        bottomLeftWidth,
+        bottomBoxHeight
+    )
+
+    love.graphics.setFont(font_medium)
+    for menuIndex, menu in ipairs(mainMenu) do
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.printf(
+            mainMenu[menuIndex],
+            menuOptionX,
+            menuOptionY + (menuIndex - 1) * menuOptionHeight,
+            menuOptionWidth,
+            'center', 0, 1, 1, 0, -1 * (menuOptionHeight/4)
+        )
+        if mainMenuHighlighted == menuIndex then
+            love.graphics.setColor(0.25, 0.25, 0.25)
+            love.graphics.rectangle(
+                'line',
+                menuOptionX,
+                menuOptionY + 5 + (menuIndex - 1) * menuOptionHeight,
+                menuOptionWidth,
+                menuOptionHeight - 5
+            )
+        end
+
+    end
+
+end
+
+function love.keypressed(key)
+    if key == 'down' and mainMenuHighlighted < #mainMenu then
+        mainMenuHighlighted = mainMenuHighlighted + 1
+    elseif key == 'up' and mainMenuHighlighted > 1 then
+        mainMenuHighlighted = mainMenuHighlighted - 1
+    end
+end
